1c301dae52c7b9185b448032a8e497d8dffd02be
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-10-19 14:20:06 +0530

    done with prisma client basics

---
+++ script.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Ahmad",
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
